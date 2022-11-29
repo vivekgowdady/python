284f474dfdb93aee96958ef428190253eb3607e6
vivekgowdady <117084240+vivekgowdady@users.noreply.github.com>
2022-11-29 16:54:07 +0530

    Create indexandslice.py

---
+++ indexandslice.py
@@ -0,0 +1,14 @@
+str ="Geeks for Geeks !"
+ 
+# slicing using indexing sequence
+print(str[: 3])
+print(str[1 : 5 : 2])
+print(str[-1 : -12 : -2])
+str ="Geeks for Geeks !"
+ 
+print("Original String :-")
+print(str)
+ 
+# reversing the string using slicing
+print("Reverse String :-")
+print(str[: : -1])# Write your code here :-)
